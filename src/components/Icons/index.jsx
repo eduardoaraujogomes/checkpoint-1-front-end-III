62a338d8cd29f2e6f35827daf828197d4f466e3d
Eduardo de Araújo <eduardoaraujogf@gmail.com>
2021-11-15 23:48:44 -0300

    Fixed some responsiveness issues of the icons

--- src/components/Icons/index.jsx
+++ src/components/Icons/index.jsx
@@ -4,7 +4,7 @@ import './style.scss';
 export default function Icons({ icon, name, description }) {
   return (
     <>
-      <div className='col-lg-2 col-2'>
+      <div className='col-lg-2 col-md-2 col-sm-0 text-center justify-sm-self-center'>
         <img className='imgSize' src={icon} alt={name} />
       </div>
       <div className='col-md-4 mb-5'>
