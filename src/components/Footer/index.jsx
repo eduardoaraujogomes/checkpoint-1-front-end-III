5c1467534637962087dc78945dd2615ecd437b94
Eduardo de Araújo <eduardoaraujogf@gmail.com>
2021-11-16 22:18:38 -0300

    Created the Footer component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './style.scss';
 
 export default function Footer() {
   return (
     <>
-      <div id='footer'>
-        <div className='container'>
+      <div id='footer' className='d-flex align-items-end'>
+        <div className='container-fluid'>
           <div className='row'>
             <div className='col-12'></div>
           </div>
